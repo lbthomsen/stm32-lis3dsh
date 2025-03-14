88b2fb62bc8a67cbb32a7ebb7522be5125d26518
Lars Boegild Thomsen <lth@netcompartner.com>
2025-03-14 20:25:45 +0800

    Started example

---
+++ src/lis3dsh.c
@@ -0,0 +1,8 @@
+/*
+ * lis3dsh.c
+ *
+ *  Created on: Mar 14, 2025
+ *      Author: lth
+ */
+
+
---
+++ src/lis3dsh.h
@@ -0,0 +1,13 @@
+/*
+ * lis3dsh.h
+ *
+ *  Created on: Mar 14, 2025
+ *      Author: lth
+ */
+
+#ifndef LIS3DSH_H_
+#define LIS3DSH_H_
+
+
+
+#endif /* LIS3DSH_H_ */
